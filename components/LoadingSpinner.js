e9771c65629764d9ee299860e3e0e8a786a3e48e
Jon Callahan <jon@joncallahan.co>
2018-09-19 10:05:33 -0700

    improves haptic feedback 🖐

--- components/LoadingSpinner.js
+++ components/LoadingSpinner.js
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {View} from 'react-native';
 import {Spinner} from 'react-native-ios-kit';
+import {Haptic} from 'expo';
 
-export const LoadingSpinner = () => (
-  <View style={{flex: 1, justifyContent: 'center'}}>
-    <Spinner theme={{primaryColor: '#666666'}} size="large" />;
-  </View>
-);
+export class LoadingSpinner extends Component {
+  componentWillUnmount() {
+    Haptic.impact(Haptic.ImpactStyles.Light);
+  }
+  render() {
+    return (
+      <View style={{flex: 1, justifyContent: 'center'}}>
+        <Spinner theme={{primaryColor: '#666666'}} size="large" />;
+      </View>
+    );
+  }
+}
